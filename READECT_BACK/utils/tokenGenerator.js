009b946f5329e911f07e0bea2d093962c726390e
ABHIRUP <abhirupsaha02@gmail.com>
2023-11-20 23:27:12 +0530

    update

--- READECT_BACK/utils/tokenGenerator.js
+++ READECT_BACK/utils/tokenGenerator.js
@@ -14,6 +14,7 @@ exports.createAndSendToken = (newReader, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + expries * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    secure: true,
   };
 
   if (environment === "production") cookieOptions.secure = true;
