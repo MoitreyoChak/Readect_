7c5bbeac6001eb8d27fc62d6031f56b8740cd5f2
ABHIRUP <abhirupsaha02@gmail.com>
2023-11-20 23:40:06 +0530

    idk

--- READECT_BACK/utils/tokenGenerator.js
+++ READECT_BACK/utils/tokenGenerator.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const expries = process.env.JWT_COOKIE_EXPIRES_IN || 900000;
-const environment = process.env.NODE_ENV || "production;";
+const environment = process.env.NODE_ENV || "production";
 
 const createToken = (id, exp) => {
   return jwt.sign({ id: id }, "my-super-secret-string-is-superb", {
@@ -14,7 +14,6 @@ exports.createAndSendToken = (newReader, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + expries * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: true,
   };
 
   if (environment === "production") cookieOptions.secure = true;
